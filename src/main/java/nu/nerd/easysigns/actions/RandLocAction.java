9683b022493a6dcabc3dc62fe2bc5ccb5b0f60aa
totemo <TotemoResearch@gmail.com>
2019-03-31 21:35:17 +1030

    Randloc should not exceed the max_distance from spawn.

--- src/main/java/nu/nerd/easysigns/actions/RandLocAction.java
+++ src/main/java/nu/nerd/easysigns/actions/RandLocAction.java
@@ -1,78 +1,74 @@
 package nu.nerd.easysigns.actions;
 
-import nu.nerd.easysigns.SignData;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-
+import nu.nerd.easysigns.SignData;
 
 public class RandLocAction extends SignAction {
 
-
-    private SignData sign;
+    private final SignData sign;
     private int distance;
     private boolean valid = true;
-
 
     public RandLocAction(SignData sign, String[] args) {
         this.sign = sign;
         try {
             distance = Integer.parseInt(args[0]);
-        } catch (NumberFormatException|IndexOutOfBoundsException ex) {
+        } catch (NumberFormatException | IndexOutOfBoundsException ex) {
             valid = false;
         }
     }
-
 
     public RandLocAction(SignData sign, ConfigurationSection attributes) {
         this.sign = sign;
         this.distance = attributes.getInt("distance");
     }
 
-
+    @Override
     public String getName() {
         return "randloc";
     }
 
-
+    @Override
     public String getUsage() {
         return "<max_distance>";
     }
 
-
+    @Override
     public String getHelpText() {
         return "Randomly spawns the player within <max_distance> away from 0,0.";
     }
 
-
+    @Override
     public String toString() {
         return String.format("%s %d", getName(), distance);
     }
 
-
+    @Override
     public boolean isValid() {
         return valid;
     }
 
-
+    @Override
     public Map<String, Object> serialize() {
         Map<String, Object> map = new HashMap<>();
         map.put("distance", distance);
         return map;
     }
 
-
+    @Override
     public void action(Player player) {
-        Random random = new Random();
-        int x = -distance + random.nextInt(distance*2);
-        int z = -distance + random.nextInt(distance*2);
+        double angle = 2 * Math.PI * Math.random();
+        double range = distance * Math.random();
+        int x = (int) (range * Math.cos(angle));
+        int z = (int) (range * Math.sin(angle));
         int y = player.getWorld().getHighestBlockYAt(x, z);
         player.teleport(new Location(player.getWorld(), x, y, z));
     }
-
 
 }
